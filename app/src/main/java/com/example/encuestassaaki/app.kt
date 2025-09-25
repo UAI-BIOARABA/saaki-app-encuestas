548153b8ef036185f0720d11f487f7e20955c85a
Andoni <ag.bioaraba@gmail.com>
2025-09-25 12:37:30 +0200

    Locale eliminado

--- app/src/main/java/com/example/encuestassaaki/app.kt
+++ app/src/main/java/com/example/encuestassaaki/app.kt
@@ -1,14 +1,10 @@
 package com.example.encuestassaaki
 
 import android.app.Application
-import android.content.Context
-import com.example.encuestassaaki.locale.LocaleHelper
 
 class App : Application() {
-
-    override fun attachBaseContext(base: Context) {
-        val sharedPref = base.getSharedPreferences("prefs", Context.MODE_PRIVATE)
-        val lang = sharedPref.getString("lang", "es") ?: "es"
-        super.attachBaseContext(LocaleHelper.setLocale(base, lang))
+    override fun onCreate() {
+        super.onCreate()
+        // Inicialización global si hace falta en el futuro
     }
 }
